5b90ac8d5fe2f97b9521855d0c3f877c0bfa5301
Mateusz Sobieraj <core.editor>
2021-08-29 17:41:16 +0200

    correct build and add markdown support

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-import Reveal from 'reveal.js'
-import 'reveal.js/dist/reveal.css'
-import 'reveal.js/dist/theme/sky.css'
+import Reveal from "reveal.js";
+import "reveal.js/dist/reveal.css";
+import "reveal.js/dist/theme/sky.css";
+import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
 
-new Reveal().initialize({ hash: true, slideNumber: true })
+new Reveal().initialize({
+  hash: true,
+  slideNumber: true,
+  plugins: [Markdown],
+});
