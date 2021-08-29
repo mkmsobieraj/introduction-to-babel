89080425986fd32bea584ee95a0664239cd409a1
Mateusz Sobieraj <core.editor>
2021-08-29 23:12:37 +0200

    Add webpack dev server, move slide logic to seperate md file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import Reveal from "reveal.js";
 import "reveal.js/dist/reveal.css";
 import "reveal.js/dist/theme/sky.css";
-import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
+import Markdown from "reveal.js/plugin/markdown/markdown.esm.js";
 
 new Reveal().initialize({
   hash: true,
-  slideNumber: true,
+  process: true,
+  transition: "convex",
   plugins: [Markdown],
 });
