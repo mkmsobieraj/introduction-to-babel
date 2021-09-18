2c594a24b0acd10acd752e7da56890462749a168
Mateusz Sobieraj <core.editor>
2021-09-19 00:05:21 +0200

    cahnge style form sky to league

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import Reveal from "reveal.js";
 import "reveal.js/dist/reveal.css";
-import "reveal.js/dist/theme/sky.css";
+import "reveal.js/dist/theme/league.css";
 import Markdown from "reveal.js/plugin/markdown/markdown.esm.js";
 import RevealNotes from "reveal.js/plugin/notes/notes.esm.js";
 import RevealHighlight from "reveal.js/plugin/highlight/highlight.esm.js";
